25351bef281162bcad3f6e2ad8431c5a03658d64
Michael Cohen <michael@fatastronauts.com>
2018-12-27 10:57:12 -0600

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+var Metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+var permalinks  = require('metalsmith-permalinks');
+
+Metalsmith(__dirname)
+  .metadata({
+    title: "We are Fat Astronauts",
+    description: "We make software"
+  })
+  .source('./src')
+  .destination('./build')
+  .clean(false)
+  .use(markdown())
+  .use(permalinks())
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .build(function(err, files) {
+    if (err) { throw err; }
+  });
